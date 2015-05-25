d980e3fcbb8cb541761e003b287744d27104258c
Darren Wong <darrenwong23@gmail.com>
2015-05-25 11:45:46 -0700

    Update instagramScrape.js

--- server/webScraping/instagramScrape.js
+++ server/webScraping/instagramScrape.js
@@ -1,23 +1,23 @@
-var ig = require('instagram-node').instagram();
-var igCredentials = require('./instagramCredentials.js');
+// var ig = require('instagram-node').instagram();
+// var igCredentials = require('./instagramCredentials.js');
 
 
-ig.use({
-	client_id: igCredentials.client_ID,
-	client_secret: igCredentials.client_secret
-});
+// ig.use({
+// 	client_id: igCredentials.client_ID,
+// 	client_secret: igCredentials.client_secret
+// });
 
-module.exports =  {
+// module.exports =  {
 	
-	getPhotosByLocation: function(req,res){
-		// ig.location_search({ lat: 48.8582, lng: 2.2945 }, {distance:100} ,function(err, result, remaining, limit) {
-		// 	res.json(result);
-		// });
+// 	getPhotosByLocation: function(req,res){
+// 		// ig.location_search({ lat: 48.8582, lng: 2.2945 }, {distance:100} ,function(err, result, remaining, limit) {
+// 		// 	res.json(result);
+// 		// });
 
-		ig.location_media_recent('16334271', function(err, result, pagination, remaining, limit) {
-			res.json(result);
-		});
-	}
-}
+// 		ig.location_media_recent('16334271', function(err, result, pagination, remaining, limit) {
+// 			res.json(result);
+// 		});
+// 	}
+// }
 
 
