b8d7fd0f927f389befcd772eb2706af9b26dd4d7
Darren Wong <darrenwong23@gmail.com>
2015-05-26 15:26:57 -0700

    fixed instagramscrape

--- server/webScraping/instagramScrape.js
+++ server/webScraping/instagramScrape.js
@@ -1,12 +1,14 @@
 var ig = require('instagram-node').instagram();
 
+	var igCredentials;
+
 if(process.env.PRODUCTION) {
 	ig.use({
 		client_id: process.env.instagram_clientID,
 		client_secret: process.env.instagram_client_secret 
 	});
 } else {
-	var igCredentials = require('./instagramCredentials.js');
+	igCredentials = require('./instagramCredentials.js');
 
 	ig.use({
 		client_id: igCredentials.instagram_client_ID,
@@ -21,10 +23,12 @@ module.exports =  {
 	getPhotosByLocation: function(req,res){
 
 		// console.log(, req.body.long);
+		console.log("req.body")
+		console.log(req.body);
 
-		ig.location_search({ lat: req.body.lat, lng: req.body.long }, {distance:50} ,function(err, result, remaining, limit) {
+		ig.location_search({ lat: req.body.lat, lng: req.body.long }, {distance:100} ,function(err, result, remaining, limit) {
 
-			// res.json(result);
+			console.log(result);
 			ig.location_media_recent(result[0].id, function(err, result, pagination, remaining, limit) {
 				res.json(result);
 			});			
