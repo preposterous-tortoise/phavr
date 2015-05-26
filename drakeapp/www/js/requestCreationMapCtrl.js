630c38fb8024963537e7edc5ab1bba13a80dc9a0
FrankBowers24 <FrankBowers24@gmail.com>
2015-05-26 14:13:24 -0700

    Added RequestCreationMapCtrl.js

---
+++ drakeapp/www/js/requestCreationMapCtrl.js
@@ -0,0 +1,18 @@
+angular.module('drakeApp.requestCreationMap', [])
+  .controller('requestCreationMapCtrl', function($scope, Favors, mapService) {
+
+    $scope.init = function() {
+      var markerMap = {};
+      var map = mapService.createMap();
+      mapService.addBoundsListener(map, markerMap);
+      mapService.addPlaceChangedListener(map);
+    }
+
+    console.log('creation view');
+    _.defer(function(){
+      var input = (document.getElementById('pac-input'));
+      console.log('creation view deferred', input);
+      $scope.init();
+    });
+
+  });
