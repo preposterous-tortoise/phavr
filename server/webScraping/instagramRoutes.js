5c8f13b07e518c24caad25dda375ff690bbc92fe
Darren Wong <darrenwong23@gmail.com>
2015-05-25 22:26:37 -0700

    fixed instagram config

--- server/webScraping/instagramRoutes.js
+++ server/webScraping/instagramRoutes.js
@@ -2,4 +2,4 @@ var instagram = require('./instagramScrape.js');
 
 module.exports = function(app) {
 	app.get('/', instagram.getPhotosByLocation);
-;}
+};
--- server/webScraping/instagramScrape.js
+++ server/webScraping/instagramScrape.js
@@ -1,15 +1,18 @@
 var ig = require('instagram-node').instagram();
-// var igCredentials = require('./instagramCredentials.js');
+var igCredentials = require('./instagramCredentials.js');
 
 
 ig.use({
-	client_id: process.env.instagram_clientID 
-	client_secret: process.env.instagram_client_secret 
+	client_id: process.env.instagram_clientID || igCredentials.instagram_client_ID,
+	client_secret: process.env.instagram_client_secret || igCredentials.instagram_client_secret 
 });
 
+
 module.exports =  {
-	
 	getPhotosByLocation: function(req,res){
+
+		console.log(req.body);
+
 		// ig.location_search({ lat: 48.8582, lng: 2.2945 }, {distance:100} ,function(err, result, remaining, limit) {
 		// 	res.json(result);
 		// });
