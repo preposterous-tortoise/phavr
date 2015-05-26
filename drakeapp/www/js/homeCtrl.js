11d4af0110e3bb7e342b91f0c870558ac451b1a9
Darren Wong <darrenwong23@gmail.com>
2015-05-25 21:28:36 -0700

    changed requests box to show correct descriptions

--- drakeapp/www/js/homeCtrl.js
+++ drakeapp/www/js/homeCtrl.js
@@ -1,27 +1,8 @@
 angular.module('drakeApp.home', [])
 .controller('homeCtrl', function ($scope, $location, Favors, photoFactory, geo){
  
-  $scope.requests = [
-    { 
-      _id: 1,
-      topic: 'LEMME SEE DRAKE',
-      description: 'hey if somebody could take a pic of drake from the front row, that would be rad',
-      photos: ["http://upload.wikimedia.org/wikipedia/en/thumb/0/01/Golden_State_Warriors_logo.svg/838px-Golden_State_Warriors_logo.svg.png"],
-      hasPhotos: false,
-      votes: 0
-    },
-    {
-      _id: 2,
-      topic: 'black tshirt',
-      description: 'take a picture of somebody wearing a black t-shirt plz',
-      photos: [],
-      hasPhotos: false,
-      votes: 0
-    }
-  ];
 
   $scope.favors = [];
-
   $scope.selectedFavor = Favors.selectedFavor;
 
   $scope.upVote = function(favor) {
