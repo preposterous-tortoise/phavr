088641de05fc6410e7c0338df4f5ee0db3d8beba
riahk <moar.riah@gmail.com>
2015-06-01 12:17:01 -0700

    testing

---
+++ drakeapp/tests/favorfactory.tests.js
@@ -0,0 +1,16 @@
+describe('favorFactory', function() {
+  var $httpBackend;
+  var $rootScope;
+
+  beforeEach(function() {
+    module('drakeApp.favorfact');
+    module('drakeApp.home');
+    module('drakeapp.photoFactory');
+    module('drakeapp.locationFactory');
+    module('ngCordovaMocks');
+    module('drakeApp.mapService');
+    module('drakeapp.authFactory');
+    module('drakeApp.navfact');
+
+  });
+
