109e8fa401c258d1bdcf07b62fe12c0d32a758e1
kaivonafsari <kaivon.afsari@gmail.com>
2015-06-01 16:16:47 -0700

    More Tests

--- server/spec/drakeapp-spec.js
+++ server/spec/drakeapp-spec.js
@@ -4,7 +4,7 @@ var frisby = require("frisby");
 
 //test might fail due to expired token, check on that.
 
-var favor
+var favorr
 
 describe("Authentication!", function () {
 
@@ -25,7 +25,7 @@ describe("Create A Favor!", function () {
 	  .expectHeader('Content-Type', 'application/json; charset=utf-8')
 	  .afterJSON(function (body) {
 	          console.log("THIS IS INSIDE BODY DOG! "+JSON.stringify(body));
-	          favor = JSON.stringify(body);
+	          favorr = JSON.stringify(body);
 	        })
 	.toss();
   
@@ -33,31 +33,75 @@ describe("Create A Favor!", function () {
 
 describe("Testing an upVote", function () {
 
-	frisby.create('Upvote')
-	  .post("http://localhost:3000/api/votes/upVote?access_token=CAAUhHz7c2VoBAHdARERGW4UkcUpCCmUnzf8oDLUyzWGlqZCKklFJa9sfwaqBkirZCsmbozPlpL0271S4NGrd76GpZACFMi6jDtcskXe85Sg46lLuyr6Yj1PtcWMi1q1xt02xGOX3IrZARMSUQaWHKNyWKORQp3u9ucNDSHFHEjHUhr8OcunU",
-	  	{ favor: {"_id":"556ca405e30393ac1cc6148f","topic":"yo","description":"yo","place_name":"Hack Reactor","address":"944 Market Street #8, San Francisco, CA 94102, United States","user_id":1,"icon":"http://frit-talk.com/mobile/2/endirect.png","votes":0,"isPrivate":false,"__v":0,"createdAt":"2015-06-01T18:27:17.510Z","loc":{"coordinates":[-122.40897799999999,37.783724],"type":"Point"},"$$hashKey":"object:14"}, vote: 1 }
+	frisby.create('Make A Favor')
+	  .post("http://localhost:3000/api/requests/create?access_token=CAAUhHz7c2VoBAHdARERGW4UkcUpCCmUnzf8oDLUyzWGlqZCKklFJa9sfwaqBkirZCsmbozPlpL0271S4NGrd76GpZACFMi6jDtcskXe85Sg46lLuyr6Yj1PtcWMi1q1xt02xGOX3IrZARMSUQaWHKNyWKORQp3u9ucNDSHFHEjHUhr8OcunU",
+	    {"topic":"lol4", "tags": "lol4", "description":"lol4","place_name":"Hack Reactor","address":"944 Market Street #8, San Francisco, CA 94102, United States","icon":"http://frit-talk.com/mobile/2/endirect.png","location":{"A": 37.783724, "F": -122.40897799999999}}
 	  	)
 	  .expectStatus(200)
-	  .expectHeader('Content-Type', 'text/html; charset=utf-8')
+	  .expectHeader('Content-Type', 'application/json; charset=utf-8')
 	  .afterJSON(function (body) {
-	          //changed values
-	          expect(body).toMatch(0);
+	  		frisby.create('Upvote')
+	  		  .post("http://localhost:3000/api/votes/upVote?access_token=CAAUhHz7c2VoBAHdARERGW4UkcUpCCmUnzf8oDLUyzWGlqZCKklFJa9sfwaqBkirZCsmbozPlpL0271S4NGrd76GpZACFMi6jDtcskXe85Sg46lLuyr6Yj1PtcWMi1q1xt02xGOX3IrZARMSUQaWHKNyWKORQp3u9ucNDSHFHEjHUhr8OcunU",
+	  		  	{ favor: {"_id": body._id}, vote: 1 }
+	  		  	)
+	  		  .expectStatus(200)
+	  		  .expectHeader('Content-Type', 'text/html; charset=utf-8')
+	  		  .afterJSON(function (body) {
+	  		          //changed values
+	  		          expect(body).toMatch(1);
+	  		        })
+	  		.toss();
+	          
 	        })
 	.toss();
+
+	// frisby.create('Upvote')
+	//   .post("http://localhost:3000/api/votes/upVote?access_token=CAAUhHz7c2VoBAHdARERGW4UkcUpCCmUnzf8oDLUyzWGlqZCKklFJa9sfwaqBkirZCsmbozPlpL0271S4NGrd76GpZACFMi6jDtcskXe85Sg46lLuyr6Yj1PtcWMi1q1xt02xGOX3IrZARMSUQaWHKNyWKORQp3u9ucNDSHFHEjHUhr8OcunU",
+	//   	{ favor: {"_id":"556ca405e30393ac1cc6148f","topic":"yo","description":"yo","place_name":"Hack Reactor","address":"944 Market Street #8, San Francisco, CA 94102, United States","user_id":1,"icon":"http://frit-talk.com/mobile/2/endirect.png","votes":0,"isPrivate":false,"__v":0,"createdAt":"2015-06-01T18:27:17.510Z","loc":{"coordinates":[-122.40897799999999,37.783724],"type":"Point"},"$$hashKey":"object:14"}, vote: 1 }
+	//   	)
+	//   .expectStatus(200)
+	//   .expectHeader('Content-Type', 'text/html; charset=utf-8')
+	//   .afterJSON(function (body) {
+	//           //changed values
+	//           expect(body).toMatch(0);
+	//         })
+	// .toss();
   
 });
 
 describe("Testing a downVote", function () {
 
-	frisby.create('downVote')
-	  .post("http://localhost:3000/api/votes/upVote?access_token=CAAUhHz7c2VoBAHdARERGW4UkcUpCCmUnzf8oDLUyzWGlqZCKklFJa9sfwaqBkirZCsmbozPlpL0271S4NGrd76GpZACFMi6jDtcskXe85Sg46lLuyr6Yj1PtcWMi1q1xt02xGOX3IrZARMSUQaWHKNyWKORQp3u9ucNDSHFHEjHUhr8OcunU",
-	  	{ favor: {"_id":"556ca405e30393ac1cc6148f","topic":"yo","description":"yo","place_name":"Hack Reactor","address":"944 Market Street #8, San Francisco, CA 94102, United States","user_id":1,"icon":"http://frit-talk.com/mobile/2/endirect.png","votes":0,"isPrivate":false,"__v":0,"createdAt":"2015-06-01T18:27:17.510Z","loc":{"coordinates":[-122.40897799999999,37.783724],"type":"Point"},"$$hashKey":"object:14"}, vote: -1 }
+	// frisby.create('downVote')
+	//   .post("http://localhost:3000/api/votes/upVote?access_token=CAAUhHz7c2VoBAHdARERGW4UkcUpCCmUnzf8oDLUyzWGlqZCKklFJa9sfwaqBkirZCsmbozPlpL0271S4NGrd76GpZACFMi6jDtcskXe85Sg46lLuyr6Yj1PtcWMi1q1xt02xGOX3IrZARMSUQaWHKNyWKORQp3u9ucNDSHFHEjHUhr8OcunU",
+	//   	{ favor: {"_id":"556ca405e30393ac1cc6148f","topic":"yo","description":"yo","place_name":"Hack Reactor","address":"944 Market Street #8, San Francisco, CA 94102, United States","user_id":1,"icon":"http://frit-talk.com/mobile/2/endirect.png","votes":0,"isPrivate":false,"__v":0,"createdAt":"2015-06-01T18:27:17.510Z","loc":{"coordinates":[-122.40897799999999,37.783724],"type":"Point"},"$$hashKey":"object:14"}, vote: -1 }
+	//   	)
+	//   .expectStatus(200)
+	//   .expectHeader('Content-Type', 'text/html; charset=utf-8')
+	//   .afterJSON(function (body) {
+	//           //changed values
+	//           expect(body).toMatch(0);
+	//         })
+	// .toss();
+
+	frisby.create('Make A Favor')
+	  .post("http://localhost:3000/api/requests/create?access_token=CAAUhHz7c2VoBAHdARERGW4UkcUpCCmUnzf8oDLUyzWGlqZCKklFJa9sfwaqBkirZCsmbozPlpL0271S4NGrd76GpZACFMi6jDtcskXe85Sg46lLuyr6Yj1PtcWMi1q1xt02xGOX3IrZARMSUQaWHKNyWKORQp3u9ucNDSHFHEjHUhr8OcunU",
+	    {"topic":"lol4", "tags": "lol4", "description":"lol4","place_name":"Hack Reactor","address":"944 Market Street #8, San Francisco, CA 94102, United States","icon":"http://frit-talk.com/mobile/2/endirect.png","location":{"A": 37.783724, "F": -122.40897799999999}}
 	  	)
 	  .expectStatus(200)
-	  .expectHeader('Content-Type', 'text/html; charset=utf-8')
+	  .expectHeader('Content-Type', 'application/json; charset=utf-8')
 	  .afterJSON(function (body) {
-	          //changed values
-	          expect(body).toMatch(0);
+	  		frisby.create('Downvote')
+	  		  .post("http://localhost:3000/api/votes/upVote?access_token=CAAUhHz7c2VoBAHdARERGW4UkcUpCCmUnzf8oDLUyzWGlqZCKklFJa9sfwaqBkirZCsmbozPlpL0271S4NGrd76GpZACFMi6jDtcskXe85Sg46lLuyr6Yj1PtcWMi1q1xt02xGOX3IrZARMSUQaWHKNyWKORQp3u9ucNDSHFHEjHUhr8OcunU",
+	  		  	{ favor: {"_id": body._id}, vote: -1 }
+	  		  	)
+	  		  .expectStatus(200)
+	  		  .expectHeader('Content-Type', 'text/html; charset=utf-8')
+	  		  .afterJSON(function (body) {
+	  		          //changed values
+	  		          expect(body).toMatch(-1);
+	  		        })
+	  		.toss();
+	          
 	        })
 	.toss();
   
