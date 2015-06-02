99a4af18f1b96d971119934eb77bbb9313d2c1db
kaivonafsari <kaivon.afsari@gmail.com>
2015-06-02 16:38:49 -0700

    Filters, some styling

--- drakeapp/www/js/mainCtrl.js
+++ drakeapp/www/js/mainCtrl.js
@@ -1,22 +1,21 @@
 angular.module('drakeApp.nav', [])
-.controller('navCtrl', function($scope, $location, $http, $ionicSideMenuDelegate, photoFactory, Auth, Nav){ 
+.controller('navCtrl', function($scope, $location, $http, $ionicSideMenuDelegate, photoFactory, Auth, Nav, Favors){ 
+  $scope.user;
+  $scope.getUserInfo = function(callback) {
+    console.log("YOLO!");
+    Favors.getUserInfo()
+      .then(function(data){
+        $scope.user = data;
+        console.log("THIS IS DATA "+data)
+        callback();
+      })
+  };
   $scope.toggleLeft = function() {
-    $ionicSideMenuDelegate.toggleLeft();
+    $scope.getUserInfo(function(){$ionicSideMenuDelegate.toggleLeft();});
+    
   };
 
-  // $scope.user;
-  $scope.getUserInfo = function() {
-    return $http({
-            method: 'GET',
-            url: 'http://drakeapp.herokuapp.com/api/profileID?access_token='+Auth.accessToken
-        })
-        .then(function(resp) {
-            console.log('response from getting server', resp);
-            $scope.user = resp;
-            return resp;
-        });
-  };
-
+  
    
 
 });
