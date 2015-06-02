cf3f032cd6d6b750a47fa8ab0222514c1ba4208c
Darren Wong <darrenwong23@gmail.com>
2015-06-02 15:14:09 -0700

    can calculate between two coordinates

--- drakeapp/www/js/locationFactory.js
+++ drakeapp/www/js/locationFactory.js
@@ -1,5 +1,5 @@
 angular.module('drakeapp.locationFactory', [])
-.factory('geo', function($cordovaGeolocation, mapService) {
+.factory('geo', function($cordovaGeolocation, mapService, Favors) {
 
 
 var processing = false;
@@ -28,6 +28,25 @@ var watchID = null;
       });
     },
 
+    calculateDistance: function(lat1, lon1, lat2, lon2, b, callback){
+
+      function deg2rad(deg) {
+        return deg * (Math.PI/180)
+      }
+
+      var R = 6371; // Radius of the earth in km
+      var dLat = deg2rad(lat2-lat1);  // deg2rad below
+      var dLon = deg2rad(lon2-lon1); 
+      var a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+        ; 
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+      var d = R * c; // Distance in km
+      return d *0.621371; //distance in miles
+    },
+
 
     enableTracking: function(callback){
       if (watchID != null) {
@@ -52,6 +71,19 @@ var watchID = null;
                 console.log( 'Latitude: '  + position.coords.latitude      + '<br />' +
                                     'Longitude: ' + position.coords.longitude     + '<br />' +
                                     '<hr />');
+
+                var radius = 0.289855/2; //= 2 miles
+                var box = [[position.coords.longitude-radius, position.coords.latitude-radius], [position.coords.longitude+radius, position.coords.latitude+radius]];
+
+                Favors.fetchRequests(box, function(data){
+                  console.log('got requests');
+                  console.log(data);
+                });
+
+
+
+
+
             }
 
                 // onError Callback receives a PositionError object
