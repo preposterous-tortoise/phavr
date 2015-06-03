df09bf504233ca0cbf3693ff9cfbf9c03474745b
Darren Wong <darrenwong23@gmail.com>
2015-06-02 22:26:35 -0700

    fixed access token

--- drakeapp/www/js/loginCtrl.js
+++ drakeapp/www/js/loginCtrl.js
@@ -24,6 +24,7 @@ angular.module('drakeApp.login', [])
 
     Auth.setAccessToken("CAAUhHz7c2VoBAHdARERGW4UkcUpCCmUnzf8oDLUyzWGlqZCKklFJa9sfwaqBkirZCsmbozPlpL0271S4NGrd76GpZACFMi6jDtcskXe85Sg46lLuyr6Yj1PtcWMi1q1xt02xGOX3IrZARMSUQaWHKNyWKORQp3u9ucNDSHFHEjHUhr8OcunU");
     $location.path('/home');
+    console.log(localStorage.getItem('token'));
 
  }
 
