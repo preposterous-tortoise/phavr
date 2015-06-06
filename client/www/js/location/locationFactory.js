369aae3c7ccf6832b85342cfde5858d9f2732c20
FrankBowers24 <FrankBowers24@gmail.com>
2015-06-05 17:29:01 -0700

    First commit of new favor notification

--- client/www/js/location/locationFactory.js
+++ client/www/js/location/locationFactory.js
@@ -39,7 +39,7 @@ angular.module('phavr.locationFactory', [])
       });
     },
 
-    updateUserLocation: function(lat, lng) {
+    updateUserLocation: function() {
       var provider_id = localStorage.getItem('user_provider_id');
       var lat = localStorage.getItem('latitude');
       var lng = localStorage.getItem('longitude');
