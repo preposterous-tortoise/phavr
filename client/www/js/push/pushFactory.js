d0ac14458f3a9706b2bd07c308bd7aae8a558cb8
FrankBowers24 <FrankBowers24@gmail.com>
2015-06-08 10:53:53 -0700

    Store notifications in localStorage

--- client/www/js/push/pushFactory.js
+++ client/www/js/push/pushFactory.js
@@ -54,6 +54,8 @@ angular.module('phavr.pushfact', [])
       }, function(err) {
         console.log("Register error " + err)
       });
+      notifications = JSON.parse(window.localStorage.getItem("notifications"));
+      notifications = notifications || [];
     }
 
     // Notification Received
@@ -89,6 +91,7 @@ angular.module('phavr.pushfact', [])
         } else {
           notifications.push(notification.message);
         }
+        window.localStorage.setItem("notifications", JSON.stringify(notifications));
         /*$scope.$apply(function () {
             $scope.notifications.push(JSON.stringify(notification.message));
         })*/
