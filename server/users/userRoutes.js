e5106584e61ff1c8ece7cce031db1a68174b1d4b
Darren Wong <darrenwong23@gmail.com>
2015-06-08 19:54:39 -0700

    background geotracking + push notifications

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -1,6 +1,16 @@
 var userController = require('./userController.js');
 
 module.exports = function (app) {
+
+	app.use('/', function(req,res, next){
+
+    req.body.lng = req.body.location.longitude;
+    req.body.lat = req.body.location.latitude;
+    req.body.timeStamp = req.body.location.recorded_at;
+    next();
+  	});
+
   app.route('/updateloc')
     .post(userController.updateLocation);
 };
+ 
